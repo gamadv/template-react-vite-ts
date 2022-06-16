f73aa0daede93ab4b07ba268ce02d19b003cfec3
gamadv <lealdev.py@gmail.com>
2022-06-16 16:35:05 -0300

    Template created

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import './styles/global.css';
+
+function App() {
+
+  return (
+    <div className="App">
+      <header className="App-header">My template header</header>
+    </div>
+  );
+}
+
+export default App;
