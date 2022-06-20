799f44516261636c8564cfbdecd5c69d08d88f67
gamadv <lealdev.py@gmail.com>
2022-06-20 10:35:11 -0300

    feat: Header component started

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,10 @@
+import ViteLogo from '../../favicon.svg';
+
+export default function Header() {
+  return (
+    <header>
+      <img src={ViteLogo} alt="Logo" />
+      My template header
+    </header>
+  );
+}
