3b6b18e87310cc7ba05664cf5c4c0f130a3476a0
gamadv <lealdev.py@gmail.com>
2022-07-25 09:47:46 -0300

    feat: header component edited; added sass

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,8 +1,12 @@
-import ViteLogo from '../../favicon.svg';
+import ViteLogo from '/favicon.svg';
+import './styles.css';
+import './styles.scss';
 
 export default function Header() {
+  const teste = 'teste';
+  console.log(teste)
   return (
-    <header>
+    <header className="headerContainer">
       <img src={ViteLogo} alt="Logo" />
       My template header
     </header>
